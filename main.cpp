1087a3442d9af4a6e11209f0d18190f465785996
adam <adamstevenson121@gmail.com>
2016-02-01 20:41:09 -0500

    more flexibility in cmd line options

--- main.cpp
+++ main.cpp
@@ -7,19 +7,21 @@ using namespace boost::asio::ip;
 using namespace std;
 
 int main(int argc, char** argv) {
-  if(argc != 3) {
-    cerr << "Usage: halfwit <server> <channel>\n";
+  if(argc != 4) {
+    cerr << "Usage: halfwit <server> <channel> <username> <password>\n";
     return 1;
   }
+  static const string SERVER = argv[1];
+  static const string CHANNEL = argv[2];
+  static const string USERNAME = argv[3];
+  static const string PASSWORD = argv[4];
   try {
     boost::asio::io_service io_service;
     tcp::resolver resolver(io_service);
-    tcp::resolver::query query (argv[1], "6667");
+    tcp::resolver::query query (SERVER, "6667");
 
-    cout << "resolving " << argv[1] << endl;
     tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
     tcp::resolver::iterator end;
-    cout << "connecting...";
     tcp::socket socket(io_service);
     boost::system::error_code error = boost::asio::error::host_not_found;
     while(error && endpoint_iterator != end) {
@@ -28,11 +30,10 @@ int main(int argc, char** argv) {
     }
     if(error)
       throw boost::system::system_error(error);
-    cout << "connected!\n";
 
     string user_msg = "USER halfwitbot myhost server :halfwitbot\n";
     string nick_msg = "NICK halfwitbot\n";
-    string join_msg = "JOIN #" + string(argv[2]) + "\n";
+    string join_msg = "JOIN #" + CHANNEL + "\n";
     socket.write_some(boost::asio::buffer(user_msg), error);
     socket.write_some(boost::asio::buffer(nick_msg), error);
     socket.write_some(boost::asio::buffer(join_msg), error);
@@ -48,7 +49,7 @@ int main(int argc, char** argv) {
       else if(error)
         throw boost::system::system_error(error);
 
-      string first_four(buf.begin(), buf.begin() + 4);
+      const string first_four(buf.begin(), buf.begin() + 4);
       if(first_four.compare("PING") == 0) {
         string pong_msg = string("PONG") + string(buf.begin() + 4, buf.end());
         socket.write_some(boost::asio::buffer(pong_msg), error);
@@ -56,7 +57,6 @@ int main(int argc, char** argv) {
           throw boost::system::system_error(error);
       }
       
-
       for(auto i: buf)
         cout << i;
     }
